021a37d298c17e00b249a2204d2a5bd0fd770c05
VicetTor <victoryvray@gmail.com>
2024-09-23 13:02:22 +0200

    feat : script v1 fonctionnel

--- script.js
+++ script.js
@@ -1,43 +1,135 @@
-let departMinutes = 1
-let temps = departMinutes * 60
-/* This JavaScript code snippet is creating a countdown timer functionality. Here's a breakdown of what
-it does: */
+let departMinutes = 25;
+let temps = departMinutes * 60;
 let estLance = 0;
-const timerElement = document.getElementById("timer");
-const button = document.getElementById("bouton");
-
+let minutes;
+let secondes;
 let departSecondes = parseInt(temps % 60);
 
-if(departSecondes < 10){
-departSecondes = '0' + departSecondes;
-}
-if(departMinutes < 10){
-departMinutes = '0' + departMinutes;
-}
-timerElement.innerText = departMinutes + ":" + departSecondes
-let tempsIntervalle;
+const timerElement = document.getElementById("timer");
+const button = document.getElementById("bouton");
+const body = document.body;
+const timerDiv = document.getElementById("timerDiv")
+const bordureNoire = document.getElementById("bordureNoire")
+const choix = document.getElementById("choix");
+
+depart();
 
 button.addEventListener("click", ()=>{
     if(estLance == 0){
         estLance = 1;
-         tempsIntervalle = setInterval(() => {
-            let minutes = parseInt(temps / 60);
-            let secondes = parseInt(temps % 60);
-            if(secondes < 10){
-                secondes = '0' + secondes;
-            }
-            if(minutes < 10){
-                minutes = '0' + minutes;
-            }
-            timerElement.innerText = minutes + ":"+ secondes;
-            temps -= 1;
-            }, 1000)
-    }
-
-    else{
-        clearInterval(tempsIntervalle);
-        timerElement.innerText = departMinutes + ":" + departSecondes
-        estLance = 0;
-        temps = departMinutes * 60
+         changerDeVueActive();
+        }
+    else if(estLance == 1){
+        clearInterval(tempsIntervalle); 
+        reinitialise();
     }
 })
+
+function reinitialise(){
+    restoreStyle();
+    departMinutes = 25;
+    timerElement.innerText = departMinutes + ":" + departSecondes;
+    estLance = 0;
+    temps = departMinutes * 60;
+}
+
+function changerDeVueActive(){
+    restoreStyle();
+    departMinutes = 25;
+    temps = departMinutes * 60;
+    departSecondes = (temps % 60);
+    
+    depart();
+
+    tempsIntervalle = setInterval(() => {
+        minutes = parseInt(temps / 60);
+        secondes = parseInt(temps % 60);
+         
+        verifySecondes();
+        verifyMinutes();
+
+        timerElement.innerText = minutes + ":"+ secondes;
+        temps -= 1;
+        
+        passageRepos();
+
+        }, 1000)
+}
+
+function changerDeVueRepos(){   
+    mettreEnVert();
+    departMinutes = 5;
+    temps = departMinutes * 60;
+    departSecondes = (temps % 60);
+    depart();
+  
+    tempsIntervalle = setInterval(() => {
+
+        minutes = parseInt(temps / 60);
+        secondes = parseInt(temps % 60);
+        
+        verifySecondes();
+        verifyMinutes();
+
+        timerElement.innerText = minutes + ":"+ secondes;
+        temps -= 1;
+        
+        passageActive();
+
+        }, 1000)
+}
+
+function mettreEnVert(){
+    body.style.backgroundColor ="green";
+    button.style.backgroundColor = "green";
+    timerDiv.style.backgroundColor = "green";
+    bordureNoire.style.border = "green 5px solid";
+    choix.style.border = "green 0.1em solid";
+    timerDiv.style.border = "8px solid black";
+}
+
+function restoreStyle(){
+    body.style.backgroundColor ="rgb(198, 48, 48)";
+    button.style.backgroundColor = "rgb(198, 48, 48)";
+    timerDiv.style.backgroundColor = "rgb(198, 48, 48)";
+    bordureNoire.style.border = "rgb(198, 48, 48) 5px solid";
+    choix.style.border = "rgb(198, 48, 48) 0.1em solid";
+    timerDiv.style.border = "8px solid #f8d58e";
+}
+
+function verifySecondes(){
+    if(secondes < 10){
+        secondes = '0' + secondes;
+    }
+}
+
+function verifyMinutes(){
+    if(minutes < 10){
+        minutes = '0' + minutes;
+    }
+}
+
+function passageRepos(){
+    if(minutes == 0 && secondes == 0){
+        clearInterval(tempsIntervalle);
+        changerDeVueRepos();
+    }
+}
+
+function passageActive(){
+    if(minutes == 0 && secondes == 0){
+        clearInterval(tempsIntervalle);
+        changerDeVueActive();
+    }
+}
+
+function depart(){
+    if(departSecondes < 10){
+    departSecondes = '0' + departSecondes;
+    }
+    if(departMinutes < 10){
+    departMinutes = '0' + departMinutes;
+    }
+    timerElement.innerText = departMinutes + ":" + departSecondes
+}
+    
